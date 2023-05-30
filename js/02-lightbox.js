10f79c6cf07b6d9532a7dd289ea4239a94131d23
Tamira2804 <tamira2804@gmail.com>
2023-05-30 21:01:12 +0200

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,27 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryRef = document.querySelector(".gallery");
+
+const createGalleryItems = ({
+  preview,
+  original,
+  description,
+}) => `<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>`;
+
+const galleryList = galleryItems.map((el) => createGalleryItems(el));
+
+galleryRef.insertAdjacentHTML("afterbegin", galleryList.join(""));
+
+const lightBox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 250,
+  loop: true,
+});
